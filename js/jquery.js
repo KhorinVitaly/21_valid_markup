37cbcb10aa36bb8bb0738f6ead75ed08b1acc85f
KhorinVitaly <khorin.v@gmail.com>
2018-03-01 11:30:59 +0600

    replace tabs

--- js/jquery.js
+++ js/jquery.js
@@ -13,8 +13,7 @@
  */
 
 (function( global, factory ) {
-
-	if ( typeof module === "object" && typeof module.exports === "object" ) {
+  if ( typeof module === "object" && typeof module.exports === "object" ) {
 		// For CommonJS and CommonJS-like environments where a proper window is present,
 		// execute the factory and get jQuery
 		// For environments that do not inherently posses a window with a document
@@ -63,10 +62,8 @@ var support = {};
 
 
 
-var
-	version = "1.11.1",
-
-	// Define a local copy of jQuery
+var version = "1.11.1",
+  // Define a local copy of jQuery
 	jQuery = function( selector, context ) {
 		// The jQuery object is actually just the init constructor 'enhanced'
 		// Need init if jQuery is called (just allow error to be thrown if not included)
